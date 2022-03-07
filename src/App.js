7807579b2e5d03783bb6d944c90c644c04c28006
Jenny Keene <jennifer0keene@gmail.com>
2022-03-06 19:04:51 -0600

    starter files/code for components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bulma/css/bulma.min.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Navbar />
+      <About />
+      <Contact />
+      <Projects />
+      <Resume />
+      <Contact />
+    </main>
   );
 }
 
