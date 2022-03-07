7e85ffad792ee6a36adcbe34e58ccf5cd975f1ca
Jenny Keene <jennifer0keene@gmail.com>
2022-03-06 19:08:07 -0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,1 +1,21 @@
-export default function About () {};
+import React from 'react';
+
+export default function About () {
+    return(
+        <section className="section" id="about">
+        <div className="section-heading">
+            <h3 className="title is-2">About Me</h3>
+            <div className="container">
+                <p>
+                    I'm a pharmacist who enjoys learning new things. 
+                    I'm always looking to expand my horizons whether it's in pharmacy or in other ventures.
+                </p>
+                <p>
+                    In my free time, I enjoy spehnding time with my family and my four dogs. I enjoy camping and mountain biking. 
+                    I also enjoy puzzles, video games, and Legos.
+                </p>
+            </div>
+        </div>
+    </section>
+    );
+};
