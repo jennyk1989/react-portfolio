45ae15fb74f521c354fdf7a0870577de023a8695
Jenny Keene <jennifer0keene@gmail.com>
2022-03-06 19:28:24 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,29 @@
-export default function Navbar() {};
+import React from "react";
+
+export default function Navbar() {
+    return(
+        <nav className="navbar is-dark is-fixed-top">
+        {/* <!-- my name --> */}
+        <div className="navbar-brand">
+            <p className="navbar-item">Jenny Keene - Portfolio</p>
+        </div>
+        {/* <!-- nav links --> */}
+        <div className="navbar-menu">
+            <div className="navbar-end">
+                <a className="navbar-item" href="#about">
+                    About Me
+                </a>
+                <a className="navbar-item" href="#projects">
+                    Work/Projects
+                </a>
+                <a className="navbar-item" href="#contact">
+                    <span>
+                        <i className="fas fa-address-card"></i>
+                    </span>
+                    <span>&nbsp Contact Me </span>
+                </a>
+            </div>
+        </div>
+    </nav>
+    );
+};
