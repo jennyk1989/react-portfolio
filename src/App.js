9f110129ac611ba40e8fee4687687adcb2579982
Jenny Keene <jennifer0keene@gmail.com>
2022-03-20 19:11:57 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bulma/css/bulma.min.css';
 import About from './components/About';
-// import Contact from './components/Contact';
+import Contact from './components/Contact';
 import Hero from './components/Hero';
 import Nav from './components/Nav';
 import Projects from './components/Projects';
@@ -16,6 +16,7 @@ function App() {
           <About />
           <Projects />        
       </main>
+      <Contact />
     </div>
     
   );
