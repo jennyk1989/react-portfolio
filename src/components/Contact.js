9966681b0fc72068c3077f1bee7d75a777354858
Jenny Keene <jennifer0keene@gmail.com>
2022-03-20 19:11:55 -0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import 'bulma/css/bulma.min.css';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <footer className="section has-background-light p-4" id="contact">
+        <div className="container">
+            
+            <div className="is-flex is-justify-content-space-around">
+                    <a className="icon-text" href="https://github.com/jennyk1989">
+                        <span className="icon">
+                            <i className="fab fa-github"/>
+                        </span>
+                        <span> GitHub </span>
+                    </a>
+                    <a className="icon-text" href="mailto:jkeenepharmd@gmail.com">
+                        <span className="icon">
+                            <i className="fas fa-envelope-square"></i>
+                        </span>
+                        <span> Email </span>
+                    </a>
+                    <a className="icon-text" href="https://www.linkedin.com/in/jennifer-keene-60aa4a225/">
+                        <span className="icon">
+                            <i className="fab fa-linkedin"></i>
+                        </span>
+                        <span>LinkedIn</span>
+                    </a>
+            </div>
+        </div>
+    </footer>
   )
 }
