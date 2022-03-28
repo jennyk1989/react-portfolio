af328db73379d1ba3831d3e03e07ff59d299697a
Jenny Keene <jennifer0keene@gmail.com>
2022-03-27 21:49:11 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,11 +8,10 @@ export default function About () {
             <h3 className="title is-2">About Me</h3>
             <div className="container">
                 <p>
-                    I'm a pharmacist who enjoys learning new things. 
-                    I'm always looking to expand my horizons whether it's in pharmacy or in other ventures.
+                    I'm a pharmacist who enjoys learning new things and I'm always looking to expand my horizons whether it's in pharmacy or in other ventures.
                 </p>
                 <p>
-                    In my free time, I enjoy spehnding time with my family and my four dogs. I enjoy camping and mountain biking. 
+                    In my free time, I enjoy spending time with my family and my four dogs. I enjoy camping and mountain biking. 
                     I also enjoy puzzles, video games, and Legos.
                 </p>
             </div>
